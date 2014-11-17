4daae6f33700f5b5258ad8d7b1af0c193091e968
Ankur Sethi <contact@ankursethi.in>
2014-11-17 20:41:23 +0530

    Imported constants and directives. Added a comment.

--- ddt/app.js
+++ ddt/app.js
@@ -7,9 +7,14 @@ require('angular-route');
 
 var app = angular.module('ddt', ['ngRoute']);
 
+require('./constants');
 require('./lib/services');
+require('./lib/directives');
 
-require('./app');
+// Importing index.js directly here because our current directory
+// contains a file called 'app.js' and a directory called 'app'.
+// Just doing a require('./app') confuses Browserify.
+require('./app/index.js');
 
 // Set up routes.
 app.config(function($routeProvider) {
@@ -18,6 +23,10 @@ app.config(function($routeProvider) {
             redirectTo: '/choose'
         })
         .when('/choose', {
+            controller: 'FontsChooserCtrl',
+            templateUrl: 'app/fontsChooser/fontsChooser.html'
+        })
+        .when('/choose/:cardType', {
             controller: 'FontsChooserCtrl',
             templateUrl: 'app/fontsChooser/fontsChooser.html'
         });
