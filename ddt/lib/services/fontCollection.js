c05db1853362df95602215d27e5786db8ce05227
Ankur Sethi <contact@ankursethi.in>
2014-11-21 13:49:12 +0530

    Fleshed out the font family collection.
        * Added constants for events broadcast by the collection.
        * Broadcast an event every time a new item is added to the
          collection.
        * Exposed a new method to generate a placeholder name for
          new families.
        * Exposed a method that returns the array of families.

--- ddt/lib/services/fontCollection.js
+++ ddt/lib/services/fontCollection.js
@@ -1,21 +1,48 @@
 'use strict';
 
 var angular = require('../angular');
+var _ = require('lodash');
 
 
-angular.module('ddt').factory('fontFamilyCollection', function() {
+var ddt = angular.module('ddt');
+
+
+ddt.constant('FontFamilyCollectionEvents', {
+    ADDED: 'fontFamilyCollection:added'
+});
+
+
+ddt.factory('fontFamilyCollection', function($rootScope, FontFamilyCollectionEvents) {
     var fontFamilies = [];
+
+    // When adding a new family, we name it "Font Family X" by default,
+    // where X is an integer. fontFamiliesCounter is X, and it's bumped
+    // up every time a new family is added.
+    var fontFamiliesCounter = 1;
+
+    var families = function() {
+        // TODO: expose this read-only.
+        return fontFamilies;
+    };
 
     var add = function(family) {
         fontFamilies.push(family);
+        $rootScope.$broadcast(FontFamilyCollectionEvents.ADDED);
+        fontFamiliesCounter++;
     };
 
     var count = function() {
-        return fontFamilies.length;
+        return _.size(fontFamilies);
+    };
+
+    var generatePlaceholderName = function() {
+        return 'New Family ' + fontFamiliesCounter.toString();
     };
 
     return {
+        families: families,
         add: add,
-        count: count
+        count: count,
+        generatePlaceholderName: generatePlaceholderName
     };
 });
