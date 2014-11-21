342413e49b71e625f0760123e9d22fcfc96e3914
Ankur Sethi <contact@ankursethi.in>
2014-11-21 13:57:50 +0530

    Fleshed out <ddt-font-card-add>.
    <ddt-font-card-add> is now able to create font families, add
    fonts to them, and add the families to the global font collection.

--- ddt/lib/directives/fontCardAdd/fontCardAddCtrl.js
+++ ddt/lib/directives/fontCardAdd/fontCardAddCtrl.js
@@ -1,10 +1,28 @@
 'use strict';
 
+var _ = require('lodash');
 
-module.exports = function ($scope) {
-    // This will be replaced with an actual FontFamily object
-    // once we add fonts to this card.
-    $scope.fontFamily = {
-        name: 'New Font Family'
+
+module.exports = function ($scope, $q, FontFamily, fontFamilyCollection) {
+    var init = function() {
+        $scope.fontFamily = {
+            name: fontFamilyCollection.generatePlaceholderName()
+        };
     };
+
+    $scope.addFontFamily = function(fontFiles) {
+        var family = new FontFamily($scope.fontFamily.name);
+        var addPromise = $q.all(_.map(fontFiles, function(file) {
+            return family.addFontFromFile(file);
+        }));
+
+        addPromise.then(function() {
+            fontFamilyCollection.add(family);
+            init();
+        }, function() {
+            // TODO: pop up an error.
+        });
+    };
+
+    init();
 };
