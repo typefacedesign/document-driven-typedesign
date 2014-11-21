090e91a502b30dd8b5d46dc5f786b582e051fbac
Ankur Sethi <contact@ankursethi.in>
2014-11-21 13:56:09 +0530

    FontFamily.addFontFromFile now uses opentype.js to load fonts.

--- ddt/lib/services/fontFamily.js
+++ ddt/lib/services/fontFamily.js
@@ -4,13 +4,32 @@ var angular = require('../angular');
 var opentype = require('opentype.js');
 
 
-angular.module('ddt').factory('FontFamily', function() {
-    var FontFamily = function(options) {
-        this.name = options.name || {};
+angular.module('ddt').factory('FontFamily', function($q) {
+    var FontFamily = function(name) {
+        if (angular.isUndefined(name)) {
+            throw new Error('No name specified for new FontFamily.');
+        }
+
+        this.name = name;
         this.fonts = [];
     };
 
-    FontFamily.prototype.addFontFromFile = function() {
+    FontFamily.prototype.addFontFromFile = function(file) {
+        var family = this;
+        var deferred = $q.defer();
+
+        var fileReader = new FileReader();
+        fileReader.onload = function(e) {
+            try {
+                family.fonts.push(opentype.parse(fileReader.result));
+                deferred.resolve();
+            } catch (e) {
+                deferred.reject({error: e, file: file});
+            }
+        };
+        fileReader.readAsArrayBuffer(file);
+
+        return deferred.promise;
     };
 
     FontFamily.prototype.addFontFromUrl = function () {
