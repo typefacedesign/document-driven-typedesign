e272cf594f79f2c049104cf2751bffbb4af8410c
Ankur Sethi <contact@ankursethi.in>
2014-11-24 20:37:15 +0530

    Removed the font card titlebar directive.

--- ddt/lib/directives/fontCardTitlebar/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-
-var angular = require('../../angular');
-angular.module('ddt').controller('FontCardTitlebarCtrl', require('./fontCardTitlebarCtrl'));
-angular.module('ddt').directive('ddtFontCardTitlebar', require('./fontCardTitlebarDirective'));
