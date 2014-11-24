1a3fd2c239af87e4e1378eebadd69948293fbe5c
Ankur Sethi <contact@ankursethi.in>
2014-11-24 20:29:39 +0530

    FontFamily objects now keep track of how font files are loaded.

--- ddt/lib/services/fontFamily.js
+++ ddt/lib/services/fontFamily.js
@@ -4,17 +4,29 @@ var angular = require('../angular');
 var opentype = require('opentype.js');
 
 
-angular.module('ddt').factory('FontFamily', function($q) {
-    var FontFamily = function(name) {
+angular.module('ddt').constant('FontFamilySources', {
+    FILE: 'file',
+    URL: 'url',
+    GOOGLE_FONTS: 'google_fonts'
+});
+
+
+angular.module('ddt').factory('FontFamily', function($q, FontFamilySources) {
+    var FontFamily = function(name, source) {
         if (angular.isUndefined(name)) {
             throw new Error('No name specified for new FontFamily.');
         }
 
         this.name = name;
+        this.source = source || FontFamilySources.FILE;
         this.fonts = [];
     };
 
     FontFamily.prototype.addFontFromFile = function(file) {
+        if (this.source !== FontFamilySources.FILE) {
+            throw new Error('Cannot add font from file to font family with source: ' + this.source);
+        }
+
         var family = this;
         var deferred = $q.defer();
 
