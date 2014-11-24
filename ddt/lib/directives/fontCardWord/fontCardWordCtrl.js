ee79ae7c2a84851b34abc3361c8de5703d0bce8d
Ankur Sethi <contact@ankursethi.in>
2014-11-24 20:34:12 +0530

    Some changes to the word font card.
        * Moved the titlebar into the font card.
        * Added the ability to add a new font to the font family.

--- ddt/lib/directives/fontCardWord/fontCardWordCtrl.js
+++ ddt/lib/directives/fontCardWord/fontCardWordCtrl.js
@@ -1,5 +1,18 @@
 'use strict';
 
+var _ = require('lodash');
 
-module.exports = function() {
+
+module.exports = function($scope, filePicker) {
+    $scope.addFont = function() {
+        filePicker.pick()
+            .then(function(files) {
+                _.each(files, function(file) {
+                    $scope.fontFamily.addFontFromFile(file)
+                        .then(function() {
+                            $scope.$digest();
+                        });
+                });
+            });
+    };
 };
