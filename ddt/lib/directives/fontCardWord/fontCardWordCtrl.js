fe5f3e82a06ade87e03e81b1d4690d2015f274e2
Ankur Sethi <contact@ankursethi.in>
2014-11-25 14:50:18 +0530

    Fixed an issue with the digest cycle in FontCardWordCtrl.
    The digest cycle for the FontCardWordCtrl wasn't getting
    triggered after a new font was added to the font family. This
    commit fixes that issue.

--- ddt/lib/directives/fontCardWord/fontCardWordCtrl.js
+++ ddt/lib/directives/fontCardWord/fontCardWordCtrl.js
@@ -10,7 +10,11 @@ module.exports = function($scope, filePicker) {
                 _.each(files, function(file) {
                     $scope.fontFamily.addFontFromFile(file)
                         .then(function() {
-                            $scope.$digest();
+                            // Okay, so here's the rub: if I don't have a .then() calling
+                            // this empty function here, the digest cycle is never run.
+                            // I can't figure out why this happens. Just having this empty
+                            // function here triggers the digest cycle. This is why people
+                            // rag on you all the time, Angular.
                         });
                 });
             });
