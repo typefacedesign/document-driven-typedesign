52780e17c23ada6902cbc8acc775fc13aad028d2
Ankur Sethi <contact@ankursethi.in>
2014-12-08 18:04:09 +0530

    Added some test strings.

---
+++ ddt/testStrings.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var angular = require('./lib/angular');
+
+
+angular.module('ddt').constant('TestParagraphs', {
+    INSECT_BITE: '' +
+        '<p>An investigation has concluded that there is little evidence that creams, painkillers and ' +
+        'anti-inflammatories often used for bites actually work. In any case, said Drug and Therapeutics Bulletin' +
+        'researchers, the reactions got better by themselves in most cases.</p>' +
+        '<p>Midges, mosquitoes, flies, fleas and bed-bugs account for most bites. A variety of remedies are sold over' +
+        'the counter in pharmacies to relieve the itching, pain and swelling. Other scientists have suggested that' +
+        'swapping thicker fur for clothes was a way of making insect bites and parasitic infestations less likely.</p>'
+});
+
+
+angular.module('ddt').constant('TestSentences', {
+    GRUMPY_WIZARDS: 'Grumpy wizards make toxic brew for the evil Queen and Jack.',
+    SIX_GUNBOATS: 'A quick movement of the enemy will jeopardize six gunboats.',
+    CHINTZY_KIDS: 'The job of waxing linoleum frequently peeves chintzy kids.',
+    PLAID_JACKETS: 'My girl wove six dozen plaid jackets before she quit.',
+    FINCH_BOX: 'Twelve ziggurats quickly jumped a finch box.'
+});
+
+
+angular.module('ddt').constant('TestWords', {
+    ARROWROOT: 'Arrowroot',
+    BARLEY: 'Barley',
+    CHERVIL: 'Chervil',
+    DUMPLING: 'Dumpling',
+    ENDIVE: 'Endive',
+    FLAXSEED: 'Flaxseed',
+    GARBANZO: 'Garbanzo',
+    HIJIKI: 'Hijiki',
+    ISHTU: 'Ishtu',
+    JICAMA: 'Jicama',
+    KALE: 'Kale',
+    LYCHEE: 'Lychee',
+    MARJORAM: 'Marjoram',
+    NECTARINE: 'Nectarine',
+    OXTAIL: 'Oxtail',
+    PIZZA: 'Pizza',
+    QUINOA: 'Quinoa',
+    ROQUEFORT: 'Roquefort',
+    SQUASH: 'Squash',
+    TOFU: 'Tofu',
+    UPPUMA: 'Uppuma',
+    VANILLA: 'Vanilla',
+    WHEAT: 'Wheat',
+    XERGIS: 'Xergis',
+    YOGURT: 'Yogurt',
+    ZWEIBACK: 'Zweiback'
+});
