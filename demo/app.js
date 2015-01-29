14553686b75097b812f7ea75d35358a51cb8d553
Ankur Sethi <contact@ankursethi.in>
2015-01-29 21:20:54 +0530

    Committing built files for the DDT demo.

--- demo/app.js
+++ demo/app.js
@@ -67,7 +67,12 @@ module.exports = function($scope, $routeParams, $location, fontFamilyCollection,
         $scope.comparisonType = $routeParams.comparisonType || FontComparisonTypes.SIDE_BY_SIDE;
         $scope.fontParameters = fontParameters.current;
         $scope.FontComparisonTypes = FontComparisonTypes;
-        $scope.comparisonMatrix = comparisonMatrix.comparisonMatrix();
+
+        $scope.$watch(function() {
+            return comparisonMatrix.comparisonMatrix();
+        }, function() {
+            $scope.comparisonMatrix = comparisonMatrix.comparisonMatrix();
+        });
     };
 
     $scope.isCurrentComparison = function(comparison) {
@@ -1327,11 +1332,12 @@ angular.module('ddt').factory('comparisonMatrix', function() {
 
     var addFamily = function(family) {
         _fontFamilies.push(family);
-        _buildComparisonMatrix();
+        _comparisonMatrix = _buildComparisonMatrix();
     };
 
     var removeFamily = function(family) {
         _.pull(_fontFamilies, family);
+        _comparisonMatrix = _buildComparisonMatrix();
     };
 
     var isAddedToComparison = function(family) {
@@ -1341,7 +1347,55 @@ angular.module('ddt').factory('comparisonMatrix', function() {
     };
 
     var _buildComparisonMatrix = function() {
-        // First, group fonts in each family by
+        if (_fontFamilies.length < 2) {
+            return [];
+        }
+
+        var groupedFontsCollection = [];
+        _.each(_fontFamilies, function(family) {
+            // Group fonts by weight.
+            var groupedFonts = _.groupBy(family.fonts, function(font) {
+                return font.weight;
+            });
+
+            // Then, group fonts in each group by sub-family name.
+            _.each(_.keys(groupedFonts), function(key) {
+                groupedFonts[key] = _.groupBy(groupedFonts[key], function(font) {
+                    return font.subFamilyName;
+                });
+            });
+
+            var groupedFontsMap = new Map();
+            _.each(_.keys(groupedFonts), function(weight) {
+                _.each(_.keys(groupedFonts[weight]), function(subFamilyName) {
+                    if (groupedFonts[weight][subFamilyName].length == 1) {
+                        groupedFontsMap.set(
+                            weight.toString() + subFamilyName,
+                            groupedFonts[weight][subFamilyName][0]
+                        );
+                    }
+                });
+            });
+
+            groupedFontsCollection.push(groupedFontsMap);
+        });
+
+        var comparisonMatrix = [];
+        groupedFontsCollection[0].forEach(function(font, property, map) {
+            var propertyExists = _.map(groupedFontsCollection, function(map) {
+                return map.has(property);
+            });
+
+            if (_.all(propertyExists)) {
+                var comparisonGroup = [];
+                _.each(groupedFontsCollection, function(map) {
+                    comparisonGroup.push(map.get(property));
+                });
+                comparisonMatrix.push(comparisonGroup);
+            }
+        });
+
+        return comparisonMatrix;
     };
 
     return {
