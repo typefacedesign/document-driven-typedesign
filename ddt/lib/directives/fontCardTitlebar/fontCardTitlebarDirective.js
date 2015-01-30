e8ce810d57522f79c80f0f8dde37bed0d03032bf
Ankur Sethi <contact@ankursethi.in>
2015-01-30 15:00:49 +0530

    Added tooltips to selection stars and added a 'Select for review' item to the card menu in the font card titlebar. Fixes issue #39.

--- ddt/lib/directives/fontCardTitlebar/fontCardTitlebarDirective.js
+++ ddt/lib/directives/fontCardTitlebar/fontCardTitlebarDirective.js
@@ -5,6 +5,10 @@ module.exports = function() {
     return {
         restrict: 'E',
         controller: 'FontCardTitlebarCtrl',
-        templateUrl: 'lib/directives/fontCardTitlebar/fontCardTitlebar.html'
+        templateUrl: 'lib/directives/fontCardTitlebar/fontCardTitlebar.html',
+        link: function(scope, element, attributes) {
+            // Initialize tooltips.
+            element.find('[data-toggle="tooltip"]').tooltip();
+        }
     };
 };
