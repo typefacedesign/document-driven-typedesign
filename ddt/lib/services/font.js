28d5442603fc8727949d4b2853266271a850483b
Ankur Sethi <contact@ankursethi.in>
2015-01-30 17:33:46 +0530

    Don't rely on the PostScript name when reading font metadata. Fixes issue #42.

--- ddt/lib/services/font.js
+++ ddt/lib/services/font.js
@@ -107,10 +107,6 @@ angular.module('ddt').factory('Font', function($q, FontSources, ErrorMessages) {
             angular.isDefined(openTypeFont.tables.name.preferredSubfamily)) {
             ddtFont.familyName = openTypeFont.tables.name.preferredFamily;
             ddtFont.subFamilyName = openTypeFont.tables.name.preferredSubfamily;
-        } else if (angular.isDefined(openTypeFont.tables.name.postScriptName)) {
-            var postScriptMetadata = openTypeFont.tables.name.postScriptName.split('-');
-            ddtFont.familyName = postScriptMetadata[0];
-            ddtFont.subFamilyName = postScriptMetadata[1];
         } else {
             ddtFont.familyName = openTypeFont.familyName;
             ddtFont.subFamilyName = openTypeFont.styleName;
