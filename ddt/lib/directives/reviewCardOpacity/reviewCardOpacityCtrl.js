905671a8c571d06c61d53267d6094777a90baf29
Ankur Sethi <contact@ankursethi.in>
2015-02-09 18:14:05 +0530

    The opacity review card now correctly mentions family names on the slider.
    Partially fixes #49.

--- ddt/lib/directives/reviewCardOpacity/reviewCardOpacityCtrl.js
+++ ddt/lib/directives/reviewCardOpacity/reviewCardOpacityCtrl.js
@@ -4,9 +4,14 @@
 module.exports = function($scope, testStrings, FontCardTypes, LETTERS) {
     var init = function() {
         $scope.opacity = 0.5;
+        $scope.inverseOpacity = 1 - $scope.opacity;
         $scope.testStrings = testStrings;
         $scope.FontCardTypes = FontCardTypes;
         $scope.letters = LETTERS;
+
+        $scope.$watch('opacity', function() {
+            $scope.inverseOpacity = 1 - $scope.opacity;
+        });
     };
 
     init();
