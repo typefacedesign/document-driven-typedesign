e79f64b26bcabd3dd1ff90f431425a576a58014c
Ankur Sethi <contact@ankursethi.in>
2015-02-19 16:43:30 +0530

    Committing built files for the DDT demo.

--- demo/app.js
+++ demo/app.js
@@ -1156,13 +1156,18 @@ angular.module('ddt').directive('ddtFontSelectorDropdown', require('./fontSelect
 'use strict';
 
 
-module.exports = function($scope) {
+module.exports = function($scope, comparisonMatrix) {
     var init = function() {
         $scope.menuVisible = false;
+        $scope.comparisonMatrix = comparisonMatrix;
     };
 
     $scope.toggleMenu = function() {
         $scope.menuVisible = !$scope.menuVisible;
+    };
+
+    $scope.removeFamily = function(family) {
+        comparisonMatrix.removeFamily(family);
     };
 
     init();
