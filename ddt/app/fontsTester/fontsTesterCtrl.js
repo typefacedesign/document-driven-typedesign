ae2f7850ba8326e57372f000de6419ed584dc366
Ankur Sethi <contact@ankursethi.in>
2015-03-24 21:07:42 +0530

    Use the FontTest service and <ddt-font-test-form> to load and display the debug font test in the testing view.

--- ddt/app/fontsTester/fontsTesterCtrl.js
+++ ddt/app/fontsTester/fontsTesterCtrl.js
@@ -1,6 +1,13 @@
 'use strict';
 
 
-module.exports = function() {
+module.exports = function($scope, FontTest) {
+    var init = function() {
+        FontTest.make('fontTests/default/')
+            .then(function(test) {
+                $scope.test = test;
+            });
+    };
 
+    init();
 };
