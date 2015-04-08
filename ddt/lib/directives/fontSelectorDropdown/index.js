e2454b7d72b1bf1a48d2c32b399037265f179c4b
Ankur Sethi <contact@ankursethi.in>
2015-04-08 16:50:33 +0530

    Removed dead code.

--- ddt/lib/directives/fontSelectorDropdown/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var angular = require('../../angular');
-
-
-angular.module('ddt').controller('FontSelectorDropdownCtrl', require('./fontSelectorDropdownCtrl'));
-angular.module('ddt').directive('ddtFontSelectorDropdown', require('./fontSelectorDropdownDirective'));
--- ddt/lib/directives/reviewCardOpacitySelector/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var angular = require('../../angular');
-
-
-angular.module('ddt').controller('ReviewCardOpacitySelectorCtrl', require('./reviewCardOpacitySelectorCtrl'));
-angular.module('ddt').directive('ddtReviewCardOpacitySelector', require('./reviewCardOpacitySelectorDirective'));
