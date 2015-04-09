e33e329f4d70b7787be1611aa3bfca6905170df6
Ankur Sethi <contact@ankursethi.in>
2015-04-09 19:04:49 +0530

    Fixed an issue where there were no letters in the letter comparison mode while in the animated toggle review mode.

--- ddt/lib/directives/reviewCardAnimatedToggle/reviewCardAnimatedToggleCtrl.js
+++ ddt/lib/directives/reviewCardAnimatedToggle/reviewCardAnimatedToggleCtrl.js
@@ -1,10 +1,11 @@
 'use strict';
 
 
-module.exports = function($scope, $timeout, testStrings, FontCardTypes) {
+module.exports = function($scope, $timeout, LETTERS, testStrings, FontCardTypes) {
     var init = function() {
         $scope.testStrings = testStrings;
         $scope.FontCardTypes = FontCardTypes;
+        $scope.letters = LETTERS;
     };
 
     init();
