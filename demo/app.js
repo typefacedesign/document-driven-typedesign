5d9b1079a6b54ff13746f24c1cc3751ad4507e33
Ankur Sethi <contact@ankursethi.in>
2015-04-20 16:47:29 +0530

    Committing built files for the DDT demo.

--- demo/app.js
+++ demo/app.js
@@ -1570,10 +1570,11 @@ angular.module('ddt').directive('ddtReviewCardAnimatedToggle', require('./review
 'use strict';
 
 
-module.exports = function($scope, $timeout, testStrings, FontCardTypes) {
+module.exports = function($scope, $timeout, LETTERS, testStrings, FontCardTypes) {
     var init = function() {
         $scope.testStrings = testStrings;
         $scope.FontCardTypes = FontCardTypes;
+        $scope.letters = LETTERS;
     };
 
     init();
