e7847097c89d1c8d0b5af484a26e77b473a2cf7b
Dave Crossland <dave@lab6.com>
2016-01-12 17:52:29 -0500

    Remove /app

--- app/components/ddt-elements/main.js
+++
@@ -1,8 +0,0 @@
-var findIndexOfKey = function(searchKey) {
-    for (var i = 0; i < localStorage.length; i++){
-        var key = localStorage.key(i);
-        if(key === searchKey)
-            return i;
-    }
-    return -1;
-}
